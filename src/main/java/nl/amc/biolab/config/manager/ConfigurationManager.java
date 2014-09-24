f03679cf1242d86112b03402655a23be30628148
Flythe <Flythe1@gmail.com>
2014-09-24 12:51:16 +0200

    added debug output

--- src/main/java/nl/amc/biolab/config/manager/ConfigurationManager.java
+++ src/main/java/nl/amc/biolab/config/manager/ConfigurationManager.java
@@ -7,7 +7,7 @@ import nl.amc.biolab.config.tools.ConfigurationWriter;
 public class ConfigurationManager {
 	public ConfigurationReader read;
 	public ConfigurationWriter writer;
-	private String config_file_path;
+	private static String config_file_path = null;
 	private String config_file_path_default = "guse/apache-tomcat-6.0.36/webapps/config.json";
 	
 	public ConfigurationManager() throws ReaderException {
@@ -15,18 +15,24 @@ public class ConfigurationManager {
 	}
 	
 	public ConfigurationManager(String config_file_path) throws ReaderException {
-		this.config_file_path = config_file_path;
+		System.out.println("Setting configuration path to: " + config_file_path);
+		
+		ConfigurationManager.config_file_path = config_file_path;
 		
 		_init();
 	}
 	
 	private void _init() throws ReaderException {
-		if (this.config_file_path == null) {
+		if (ConfigurationManager.config_file_path == null) {
 			// Set to default
-			this.config_file_path = config_file_path_default;
+			System.out.println("Setting configuration path to default: " + config_file_path_default);
+			
+			ConfigurationManager.config_file_path = config_file_path_default;
 		}
 		
-		this.read = new ConfigurationReader(this.config_file_path);
+		System.out.println("Reading from: " + config_file_path);
+		
+		this.read = new ConfigurationReader(ConfigurationManager.config_file_path);
 		this.writer = new ConfigurationWriter(this.read);
 	}
 }
